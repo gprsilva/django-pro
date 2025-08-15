aaec95e13e07995a1643ffb588e0c39a157a979e
gprsilva <gprsilva2008@gmail.com>
2025-08-15 16:12:27 -0300

    teste professor2

--- webdev/settings.py
+++ webdev/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static'] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # pasta para coleta dos arquivos
 
